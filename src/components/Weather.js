a4a444d491116717bcbcc3367f8db9c637af6d57
ycmeal <85287040+ycmeal@users.noreply.github.com>
2025-05-30 12:24:13 +0300

    Handle missing city by showing error

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,6 +4,7 @@ class Weather extends React.Component {
     render() {
         return (
             <div>
+                {this.props.error && <p>{this.props.error}</p>}
                 { this.props.city &&
                     <>
                     <p>Local: {this.props.city}, {this.props.country}</p>
